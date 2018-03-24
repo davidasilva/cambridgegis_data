59fd1a39c246c6e15ea4ce4572ff6f4c2c216780
Davi da Silva <davi@uchicago.edu>
2018-03-24 14:21:33 -0400

    adding files object
    get_directory_structure modified form here:
    https://github.com/ActiveState/code/blob/3b27230f418b714bc9a0f897cb8ea189c3515e99/recipes/Python/577879_Create_nested_dictionary/recipe-577879.py

---
+++ __init__.py
@@ -0,0 +1,50 @@
+from os.path import dirname
+
+def get_directory_structure(rootdir):
+    """
+    Creates a nested dictionary that represents the folder structure of rootdir
+    """
+    dir = {}
+    rootdir = rootdir.rstrip(os.sep)
+    start = rootdir.rfind(os.sep) + 1
+    for path, dirs, files in os.walk(rootdir):
+        folders = path[start:].split(os.sep)
+        files = [path + os.sep + f for f in files]
+        subdir = dict.fromkeys(files)
+
+        
+        
+
+        parent = reduce(dict.get, folders[:-1], dir)
+        parent[folders[-1]] = subdir
+    return dir
+
+class filetree(object):
+    def __init__(self, **response):
+        for k,v in response.items():
+            if isinstance(v,dict):
+                k = k.lstrip('.')
+                if k[0] in '1234567890':
+                    k = 'dir_' + k
+                self.__dict__[k.lstrip('.')] = filetree(**v)
+            elif isinstance(k,str):
+                try:
+                    extension=None
+                    fname,extension = k.split(os.sep)[-1].split('.')
+
+                    self.__dict__[extension] = k
+                except:
+                    self.__dict__['files'] = [k]
+
+            else:
+                 self.__dict__[k.lstrip('.')] = v
+    def __repr__(self):
+        return '< Directory with: ' + str(self.__dict__.keys()) + ' >'
+
+    def __str__(self):
+        return self.__repr__()
+    
+    
+path = dirname(__file__)
+path_dict = get_directory_structure(path).values()[0]
+files = filetree(**path_dict)
